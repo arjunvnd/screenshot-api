2e49e71caf7715ef3b15cace0603ec47a962c0a5
user <arjunvnd@gmail.com>
2020-12-05 18:24:14 +0530

    ui integration basic

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
 const puppeteer = require("puppeteer");
-const port=process.env.PORT||3001
-const path = require('path')
+const port = process.env.PORT || 3001;
+const path = require("path");
+app.use(cors());
+app.options('*', cors());
 
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
-
 app.get("/api/screenshot", async (req, res) => {
+
   const { weburi } = req.query;
   try {
     const browser = await puppeteer.launch();
@@ -19,10 +20,15 @@ app.get("/api/screenshot", async (req, res) => {
     const image = await page.screenshot({ fullPage: true });
     await browser.close();
     res.set("Content-Type", "image/png");
+    res.status(200)
     res.send(image);
   } catch (error) {
     console.log(error);
   }
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(port, () => {
