dd1629bb1a693ac514206897db12134c243c6ea2
user <arjunvnd@gmail.com>
2020-12-05 22:11:18 +0530

    error messages

--- index.js
+++ index.js
@@ -17,15 +17,18 @@ app.get("/api/screenshot", async (req, res) => {
     const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
     await page.waitFor(500);
+    await page.setDefaultNavigationTimeout(0);
+
     await page.goto(weburi);
     if (width && height) await page.setViewport({width:Number(width),height:Number(height)});
+    console.log('Taking the screenshot ')
     const image = await page.screenshot({ fullPage: true, type: imagetype });
     await browser.close();
     res.set("Content-Type", "image/png");
     res.status(200);
     res.send(image);
   } catch (error) {
-    console.log(error);
+    console.log("Error in screenshots api",error);
     res.status(400).send(error);
   }
 });
